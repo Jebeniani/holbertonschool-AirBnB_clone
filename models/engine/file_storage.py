760af5ac98eebce2bd3cd551dba4769b2ac73ff8
jebeniani <aymenjebeniani05@gmail.com>
2022-07-01 00:56:43 -0700

    updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,31 +2,33 @@
 """class FileStorage"""
 import json
 from models.base_model import BaseModel
+from os import path
+
 
 class FileStorage:
         __file_path = "path.json"
         __objects = {}
 
         def all(self):
-            return self.objects
+            return self.__objects
 
         def new(self,obj):
             """sets in __objects the obj with key <obj class name>.id"""
-            our_key = "{}.{}".format(type(obj).name, obj.id
-            self.__objects[our_key] = obj
+            key = obj.__class__.__name__ + '.' + obj.id
+            self.__objects[key] = obj
 
         def save(self):
             """serializes __objects to the JSON file (path: __file_path"""
             dictionary = {}
-            for our_key, value in self.__objects.items():
-                dictionary[our_key] = value.to_dict()
+            for key, value in self.__objects.items():
+                dictionary[key] = value.to_dict()
             with open(self.__file_path, "w", encoding='utf-8') as AJ:
-                json.dumps(dictionary, AJ)
+                AJ.write(json.dumps(dictionary))
 
         def reload(self):
             """deserializes the JSON file to __objects"""
-            try:
+            if path.exists(self.__file_path):
                 with open(self.__file_path, mode='r', encoding='utf-8') as AJ:
-                    self.__objects = json.load(AJ)
-            except IOError:
-                pass
+                    dictionary = json.loads(AJ.read())
+                    for key, value in dictionary.items():
+                        self.__objects[key] = eval(value['__class__'])(**value)
