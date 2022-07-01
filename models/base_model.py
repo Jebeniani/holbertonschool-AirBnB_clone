70608e2a726db81fc69b107a45277b9d21e6a9c5
jebeniani <aymenjebeniani05@gmail.com>
2022-07-01 00:55:53 -0700

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 from datetime import datetime
-import uuid
-
+from uuid import uuid4
+import models
 
 class BaseModel:
-
+    """a class named BaseModel"""
 
     def __init__(self, *args, **kwargs):
+        """initializs an instance of BaseModel"""
         if kwargs:
             for arg, val in kwargs.items():
                 if arg in ('created_at', 'updated_at'):
@@ -16,39 +17,27 @@ class BaseModel:
                 if arg != '__class__':
                     setattr(self, arg, val)
         else:
-            self.id = str(uuid.uuid4())
-            self.updated_at = str(datetime.now())
-            self.created_at = str(datetime.now())
-
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
-        myDict = self.__dict__
-        return f"{self.__class__.__name__} ({self.id}) {myDict}"
+
+        return '[{}] ({}) {}'.format(
+                self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = str(datetime.now())
+        """updates a BaseModel instance"""
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        return self.__dict__
+        """returns a dictionary containing all keys/values
+        of __dict__ of the instance"""
+        my_dict = self.__dict__.copy()
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
 
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-print(my_model.id)
-print(my_model)
-print(type(my_model.created_at))
-print("--")
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
-print("--")
-my_new_model = BaseModel(**my_model_json)
-print(my_new_model.id)
-print(my_new_model)
-print(type(my_new_model.created_at))
-
-print("--")
-print(my_model is my_new_model)
+        return my_dict
