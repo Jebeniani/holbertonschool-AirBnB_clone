79414a8d3e9ce1c8be7d0305c727114d5d4808e7
Aleph235 <akram239911@gmail.com>
2022-07-02 19:37:35 +0200

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -3,14 +3,12 @@
 
 from datetime import datetime
 from uuid import uuid4
-import models
 
 
 class BaseModel:
     """a class named BaseModel"""
 
     def __init__(self, *args, **kwargs):
-        """initializs an instance of BaseModel"""
         if kwargs:
             for arg, val in kwargs.items():
                 if arg in ('created_at', 'updated_at'):
@@ -25,21 +23,17 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
-        return '[{}] ({}) {}'.format(
-                self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """updates a BaseModel instance"""
         self.updated_at = datetime.now()
         models.storage.save()
 
+
     def to_dict(self):
-        """returns a dictionary containing all keys/values
-        of __dict__ of the instance"""
         my_dict = self.__dict__.copy()
         my_dict['__class__'] = self.__class__.__name__
         my_dict['created_at'] = self.created_at.isoformat()
         my_dict['updated_at'] = self.updated_at.isoformat()
-
         return my_dict
