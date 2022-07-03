25b1ff527b2c308007ecd4c11762bd0aba940722
jebeniani <aymenjebeniani05@gmail.com>
2022-07-03 14:22:39 -0700

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,14 @@
 
 from datetime import datetime
 from uuid import uuid4
+import models
 
 
 class BaseModel:
     """a class named BaseModel"""
 
     def __init__(self, *args, **kwargs):
+        """__init__ method for BaseModel"""
         if kwargs:
             for arg, val in kwargs.items():
                 if arg in ('created_at', 'updated_at'):
@@ -23,15 +25,20 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
-
+        """the class reprresentation to the user"""
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """updates the public instance attribute
+        updated_at with the current datetime"""
         self.updated_at = datetime.now()
         models.storage.save()
 
 
     def to_dict(self):
+        """ returns a dictionary containing
+        all keys/values of __dict__ of the instance"""
         my_dict = self.__dict__.copy()
         my_dict['__class__'] = self.__class__.__name__
         my_dict['created_at'] = self.created_at.isoformat()
